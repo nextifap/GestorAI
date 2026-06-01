62971307c1b667d67dc7abb13ec3e06dafafca99
CAIO - NEW PC <caiocgmsgracopi@gmail.com>
2026-05-31 22:38:16 -0300

    Pega mensagens dos ultimos meses

--- src/app/(api)/api/chat/[conversationId]/route.js
+++ src/app/(api)/api/chat/[conversationId]/route.js
@@ -6,6 +6,9 @@ import { errorResponse, respondAuthError } from '@/lib/apiErrors';
 
 // Rota GET para buscar todas as mensagens de uma conversa
 export async function GET(request, { params }) {
+
+  params = await params;
+
   const verificacao = verifyRequestToken(request);
   if (verificacao.status !== 200) {
     return respondAuthError(verificacao);
@@ -13,20 +16,42 @@ export async function GET(request, { params }) {
 
   const { id: userId } = verificacao.usuario;
   const conversationId = params.conversationId;
+  const isPooling = params.isPooling === 'true' || params.isPooling === true; // Parâmetro para indicar se é uma requisição de pooling (atualização periódica)
 
   try {
-    // Buscar a conversa e suas mensagens e verificar se ela pertence ao usuário logado
+    // 1. Calcula a data de 3 meses atrás
+    const seisMesesAtras = new Date();
+    seisMesesAtras.setMonth(seisMesesAtras.getMonth() - 6);
+
+    // 2. Busca a conversa com o filtro de tempo e limite
     const conversationWithMessages = await prisma.conversation.findFirst({
       where: {
         id: conversationId,
         userId: userId,
+        ...(isPooling && { newMessages: true }),
       },
       include: {
         messages: {
-          orderBy: { createdAt: 'asc' }, // Ordena as mensagens em ordem cronológica
+          where: {
+            createdAt: {
+              gte: seisMesesAtras, //mensagens criadas de 3 meses atrás até hoje
+            },
+          },
+          orderBy: { createdAt: 'desc' }, //  Pega as mais recentes primeiro para o 'take' não cortar as erradas
+          take: 100, //Trava de segurança: traz no máximo 150 mensagens
         },
       },
     });
+
+    await prisma.conversation.update({
+      where: { id: conversationId },
+      data: { newMessages: false }, // Marca que as mensagens foram lidas/visualizadas
+    });
+
+    // 3. Reorganiza na ordem cronológica (antiga -> nova) para o chat fazer sentido na tela
+    if (conversationWithMessages && conversationWithMessages.messages) {
+      conversationWithMessages.messages.reverse();
+    }
 
     if (!conversationWithMessages) {
       return errorResponse('CHAT_CONVERSATION_NOT_FOUND');
